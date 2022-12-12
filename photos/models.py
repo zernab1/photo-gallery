4c8c074a20cc0d7665d7893c5b6c80b6e2a28d9e
Zernab Gohar <zernab.gohar@raytheon.com>
2022-12-12 16:46:41 -0500

    sfas

--- photos/models.py
+++ photos/models.py
@@ -26,7 +26,6 @@ class Photo(models.Model):
     topic = models.ForeignKey(
         Topic, on_delete=models.SET_NULL, null=True, blank=True)
     
-    user_name = topic.user.username
     image = models.ImageField(null=False, blank=False)
     created_on = models.DateTimeField(default=timezone.now)
     description = models.TextField()
